0ce60676ac5432ddfcdb91213e1307e6511cb7bb
Evgenii-Vilkel <evgeniivilkel@gmail.com>
2025-08-12 22:18:15 +0300

    feat: add leaflet draw

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,21 +1,45 @@
-import React from 'react';
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
-import './App.css';
+import React, { useEffect } from 'react';
+import { MapContainer, TileLayer, useMap } from 'react-leaflet';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-draw/dist/leaflet.draw.css';
+import 'leaflet-draw';
 
-function App() {
+function DrawControl() {
+  const map = useMap();
+
+  useEffect(() => {
+    if (!map) return;
+
+    const drawnItems = new L.FeatureGroup();
+    map.addLayer(drawnItems);
+
+    const drawControl = new L.Control.Draw({
+      edit: {
+        featureGroup: drawnItems
+      }
+    });
+
+    map.addControl(drawControl);
+
+    map.on(L.Draw.Event.CREATED, (event: any) => {
+      const layer = event.layer;
+      drawnItems.addLayer(layer);
+    });
+
+  }, [map]);
+
+  return null;
+}
+
+export default function App() {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+    <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: "100vh" }}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      <DrawControl />
     </MapContainer>
   );
 }
-
-export default App;
